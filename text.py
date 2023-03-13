c1b27fadbe85f4a66ff1c7ef5f144d2bceba945e
odundlaw <odunayoshittu55@gmail.com>
2023-03-13 01:37:26 +0100

    welraus continue

--- text.py
+++ text.py
@@ -15,3 +15,19 @@ users = [
 for user in users:
     if ((name := user.get('name')) is not None):
         print(f"{name}")
+
+f = lambda s: s**2
+
+dat = [1, 2, 3, 4, 5, 6]
+
+print(sum(dat))
+
+filtered_power_dat = [g for s in dat if((g := f(s)) > 8)]
+print(filtered_power_dat)
+
+
+dicto = {"A" : [40, 62, 65], "B": [35, 62, 70, 82], "C": [28, 45, 80], "D": [91, 77]}
+
+q = {g: mean for g in dicto if((mean := (sum(dicto[g]) // len(dicto[g])))) > 60}
+
+print(q)
